446922526570d03dd237226a74b242c0170a541f
Saihaj Mann <saihajmann07@gmail.com>
2024-08-11 17:28:17 -0600

    basic

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import Weather from "@/components/weathercomponentbasic";
 
 export default function Home() {
   return (
     <main>
       Weather Web
+      <Weather/>
     </main>
   );
 }
