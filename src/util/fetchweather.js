38d84edd27c7afcb470ffa69c94623bf1368164b
Saihaj Mann <saihajmann07@gmail.com>
2024-08-11 17:14:22 -0600

    testing use vsc for commits

---
+++ src/util/fetchweather.js
@@ -0,0 +1,13 @@
+export default async function fetchWeather(city) {
+    const apiKey = process.env.NEXT_PUBLIC_OPENWEATHER_API_KEY;
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+    );
+  
+    if (!response.ok) {
+      throw new Error('Failed to fetch weather data');
+    }
+  
+    return response.json();
+  }
+  
