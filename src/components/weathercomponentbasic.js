8a6b02e26ec8c636295143ea4722d9dab22dfdfc
Saihaj Mann <saihajmann07@gmail.com>
2024-08-12 17:34:29 -0600

    better data displaying with time

--- src/components/weathercomponentbasic.js
+++ src/components/weathercomponentbasic.js
@@ -2,6 +2,7 @@
 
 import { useState } from 'react';
 import fetchWeather from './util/fetchweather';
+import moment from 'moment-timezone';
 
 export default function Weather() {
   const [weather, setWeather] = useState(null);
@@ -13,12 +14,24 @@ export default function Weather() {
   async function getWeatherInfo() {
     try {
       const data = await fetchWeather(city);
-      setWeather(data);
-      setOldCity(city);
-      setSearches((prevSearches) => [{ city, data }, ...prevSearches]);//Add new searches first
+      setWeather(data);//Set current data
+      setOldCity(city);//Set current city
+
+      //Save time
+      const now = moment();
+      const timeString = now.format('h:mm:ss A'); // Format time as hh:mm:ss AM/PM
+      const abbr = now.tz(moment.tz.guess()).format('z');
+      const timeNow = timeString + " " + abbr
+
+      setSearches((prevSearches) => [{ city, data, timeNow }, ...prevSearches]);//Add new searches first
     } catch (err) {
       setError(err.message);
     }
+  }
+
+  function displayAll(data){
+    console.log(data)//Print all data to console
+    alert(data)
   }
 
   return (
@@ -41,8 +54,10 @@ export default function Weather() {
       {searches.map((item, index) => (
         <div key={index} className="search-result">
           <h3>{item.city}</h3>
+          <p>Time: {item.timeNow}</p>
           <p>Temperature: {item.data.main.temp}°C</p>
           <p>Condition: {item.data.weather[0].description}</p>
+          <button onClick={()=>displayAll(item)}>All data</button>
         </div>
       ))}
     </div>
