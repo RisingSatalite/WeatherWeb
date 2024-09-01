fca6c95de68802c065ffe8e086b8f78bf623871e
RisingSatalite <107381820+RisingSatalite@users.noreply.github.com>
2024-09-01 16:26:11 -0600

    Update leafletmap.js
    marker non image change

--- src/components/util/leafletmap.js
+++ src/components/util/leafletmap.js
@@ -5,14 +5,11 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css'; //Very important for map to look and be usable
 
 // Create a custom icon
-const customIcon = new L.Icon({
-  iconUrl: require('path/to/your/icon.png'), // Path to your custom marker image
-  iconSize: [25, 41], // Size of the icon
-  iconAnchor: [12, 41], // Anchor the icon
-  popupAnchor: [1, -34], // Anchor for the popup
-  shadowUrl: require('path/to/your/marker-shadow.png'), // Optional shadow image
-  shadowSize: [41, 41],
-  shadowAnchor: [12, 41]
+const customDivIcon = new L.DivIcon({
+  html: '<div style="background-color: #000; color: #fff; padding: 5px; border-radius: 5px;">Custom Marker</div>',
+  className: 'custom-div-icon',
+  iconSize: [30, 42],
+  iconAnchor: [15, 42]
 });
 
 export default function LeafletMap({ positions }) {
@@ -25,8 +22,8 @@ export default function LeafletMap({ positions }) {
                     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 />
                 {positions.map((position, index) => (
-                    <Marker key={index} position={[position.lat, position.lng]} icon={customIcon}>
-                        <Popup>{label}</Popup>
+                    <Marker key={index} position={[position.lat, position.lng]} icon={customDivIcon}>
+                      <Popup>{label}</Popup>
                     </Marker>
                 ))}
             </MapContainer>
