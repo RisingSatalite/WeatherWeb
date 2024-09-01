e293985cdcd9eae381bcd5c16efe066c2ecaf302
RisingSatalite <107381820+RisingSatalite@users.noreply.github.com>
2024-09-01 16:22:01 -0600

    Update leafletmap.js
    better marker style

--- src/components/util/leafletmap.js
+++ src/components/util/leafletmap.js
@@ -4,6 +4,17 @@ import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css'; //Very important for map to look and be usable
 
+// Create a custom icon
+const customIcon = new L.Icon({
+  iconUrl: require('path/to/your/icon.png'), // Path to your custom marker image
+  iconSize: [25, 41], // Size of the icon
+  iconAnchor: [12, 41], // Anchor the icon
+  popupAnchor: [1, -34], // Anchor for the popup
+  shadowUrl: require('path/to/your/marker-shadow.png'), // Optional shadow image
+  shadowSize: [41, 41],
+  shadowAnchor: [12, 41]
+});
+
 export default function LeafletMap({ positions }) {
 
     return (
@@ -14,8 +25,8 @@ export default function LeafletMap({ positions }) {
                     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 />
                 {positions.map((position, index) => (
-                    <Marker key={index} position={[position.lat, position.lng]}>
-                        <Popup>{position.label}</Popup>
+                    <Marker key={index} position={[position.lat, position.lng]} icon={customIcon}>
+                        <Popup>{label}</Popup>
                     </Marker>
                 ))}
             </MapContainer>
